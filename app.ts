33da9a3b71e560c45698e6891c088def6f5f5f45
rajaXcodes <konig.9122005@gmail.com>
2025-07-13 17:37:15 +0530

    updated the url

--- app.ts
+++ app.ts
@@ -10,17 +10,19 @@ import login from "./src/routes/login";
 const app = express();
 
 app.use(morgan("dev"));
-app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173/' || '*',
-  credentials : true,
-  optionsSuccessStatus : 200
-}));
+app.use(
+  cors({
+    origin : process.env.FRONTEND_URL || `http://localhost:5173/`, // Remove any undefined values
+    credentials: true,
+    optionsSuccessStatus: 200,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("", login);
 app.use("/interview", startInterview);
-app.use("/interview",interviewRoute);
+app.use("/interview", interviewRoute);
 
 app.use("/interview", endhere);
 app.use("/map", clearmap);
