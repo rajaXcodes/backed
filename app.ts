72789319ab8b288fba8d7b11aed28984dd5c49fd
rajaXcodes <konig.9122005@gmail.com>
2025-07-13 17:21:50 +0530

    added frontEnd url in env

--- app.ts
+++ app.ts
@@ -10,19 +10,17 @@ import login from "./src/routes/login";
 const app = express();
 
 app.use(morgan("dev"));
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173/" || "*",
-    credentials: true,
-    optionsSuccessStatus: 200,
-  })
-);
+app.use(cors({
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173/' || '*',
+  credentials : true,
+  optionsSuccessStatus : 200
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("", login);
 app.use("/interview", startInterview);
-app.use("/interview", interviewRoute);
+app.use("/interview",interviewRoute);
 
 app.use("/interview", endhere);
 app.use("/map", clearmap);
